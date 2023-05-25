7ad070a8be9903a7a439283680acbc7bf22ecc14
Javier Atiencia <sldnrpl8@gmail.com>
2023-05-25 12:21:27 +0200

    Creando API para mostrar categorias y productos

--- backend/app/Http/Controllers/CategoryController.php
+++ backend/app/Http/Controllers/CategoryController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\CategoryCollection;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
     public function index(){
-        return response()->json(['categories'=>Category::all()]);
+        //return response()->json(['categories'=>Category::all()]);
+        return new CategoryCollection(Category::all());
     }
 
 }
